1528a0364e92c72aff6421766ea590deb546cce6
Eric Jarvies <eric@csl.mx>
2019-02-11 13:03:05 -0800

    Added screen blur during switch mode

--- multichain/AppDelegate.swift
+++ multichain/AppDelegate.swift
@@ -21,6 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = window!.frame
+        blurEffectView.tag = 221122
+
+        self.window?.addSubview(blurEffectView)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -30,10 +37,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
+        self.window?.viewWithTag(221122)?.removeFromSuperview()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+        self.window?.viewWithTag(221122)?.removeFromSuperview()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
